d5145b362dcfabcddfc708864d1f250e961203ed
Rosa <rmmedina@florida-uni.es>
2017-09-19 14:36:52 +0200

    subiendo proyecto pruebas

---
+++ pruebas/src/pruebas/Ppal.java
@@ -0,0 +1,14 @@
+package pruebas;
+
+public class Ppal {
+
+	public static void main(String[] args) {
+		float num=2.1f;
+		float num2=2.2f;
+		double num3=2.3;
+		System.out.println("num1 "+num);
+		System.out.println("num2 "+num2);
+		System.out.println("num3 "+num3);
+	}
+
+}
